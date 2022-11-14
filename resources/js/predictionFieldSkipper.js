ca4ffc29603a98b1745451d60c08d4b3f2d55c38
Tom Lynch <tom@lnch.co.uk>
2022-11-14 16:34:41 +0000

    Add more granularity to the key skipper

--- resources/js/predictionFieldSkipper.js
+++ resources/js/predictionFieldSkipper.js
@@ -3,9 +3,19 @@ document.addEventListener('DOMContentLoaded', function (event) {
     predictionFields.forEach(function (field) {
         const tabIndex = field.getAttribute('tabindex');
         field.addEventListener('keyup', function (event) {
-            const newField = document.querySelector('input[data-prediction-field][tabindex="' + (parseInt(tabIndex) + 1) + '"]')
+            let newField = null
+
+            if (event.key === 'ArrowLeft') {
+                newField = document.querySelector('input[data-prediction-field][tabindex="' + (parseInt(tabIndex) - 1) + '"]')
+            } else if (event.key === 'ArrowRight') {
+                newField = document.querySelector('input[data-prediction-field][tabindex="' + (parseInt(tabIndex) + 1) + '"]')
+            } else if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(event.key)) {
+                newField = document.querySelector('input[data-prediction-field][tabindex="' + (parseInt(tabIndex) + 1) + '"]')
+            }
+
             if (newField) {
                 newField.focus()
+                newField.select()
             }
         });
     });
