5cbd785fc1868412567297b4c8de460993ec76b3
Tom Lynch <tom@lnch.co.uk>
2023-01-30 14:48:23 +0000

    Add an actual space

--- resources/views/vendor/mail/html/layout.blade.php
+++ resources/views/vendor/mail/html/layout.blade.php
@@ -41,7 +41,7 @@ width: 100% !important;
 @isset($preheader)
 <td>
 <span class="preheader" style="display: none !important; visibility: hidden; opacity: 0; color: transparent; height: 0; width: 0;">
-{{ str_pad($preheader, 80, '͏') }}
+{{ str_pad($preheader, 80, ' ') }}
 </span>
 </td>
 @endisset
