bdc2074da068fe0c597746dc69bb08327474bffb
Tom Lynch <tom@lnch.co.uk>
2023-02-24 21:03:33 +0000

    Add null checks to fixture detail

--- app/Http/Controllers/FixtureDetailController.php
+++ app/Http/Controllers/FixtureDetailController.php
@@ -16,11 +16,16 @@ class FixtureDetailController extends Controller
      */
     public function __invoke(Request $request, Fixture $fixture): Renderable
     {
-        $homeColour = $fixture->lineups()->where('team_id', $fixture->home_team_id)->first()->colours['player']['primary'];
-        $awayColour = $fixture->lineups()->where('team_id', $fixture->away_team_id)->first()->colours['player']['primary'];
+        $homeLineup = $fixture->lineups()->where('team_id', $fixture->home_team_id)->first();
+        $homeColour = $homeLineup ? $homeLineup->colours['player']['primary'] : null;
+
+        $awayLineup = $fixture->lineups()->where('team_id', $fixture->away_team_id)->first();
+        $awayColour = $awayLineup ? $awayLineup->colours['player']['primary'] : null;
 
         return view('fixtures.detail', [
             'fixture' => $fixture,
+            'homeLineup' => $homeLineup,
+            'awayLineup' => $awayLineup,
             'homeColour' => $homeColour,
             'awayColour' => $awayColour,
             'homeStats' => $fixture->statistics()->where('team_id', $fixture->home_team_id)->first(),
--- resources/views/fixtures/detail.blade.php
+++ resources/views/fixtures/detail.blade.php
@@ -16,41 +16,46 @@
             <div class="flex gap-x-4">
                 <div class="w-1/3">
                     <x-panel>
-{{--                        @dump($fixture->lineups)--}}
                         <div class="flex justify-between">
-                            <img src="{{ $fixture->lineups->first()->team->logo }}" alt="" />
-                            <img src="{{ $fixture->lineups->last()->team->logo }}" alt="" />
+                            <img src="{{ $fixture->homeTeam->logo }}" alt="" />
+                            <img src="{{ $fixture->awayTeam->logo }}" alt="" />
                         </div>
                     </x-panel>
 
                     <div class="h-4"></div>
 
                     <x-panel>
-                        @foreach ($fixture->events as $event)
+                        @forelse ($fixture->events as $event)
                             @include('fixtures.partials.fixture-event', ['event' => $event, 'fixture' => $fixture])
-                        @endforeach
+                        @empty
+                            No events found yet
+                        @endforelse
                     </x-panel>
                 </div>
 
                 <div class="w-2/3">
                     <x-panel>
-                        @include('fixtures.partials.stat-bar', ['title' => 'Shots on Goal', 'statField' => 'shots_on_goal'])
-                        @include('fixtures.partials.stat-bar', ['title' => 'Shots off Goal', 'statField' => 'shots_off_goal'])
-                        @include('fixtures.partials.stat-bar', ['title' => 'Total Shots', 'statField' => 'total_shots'])
-                        @include('fixtures.partials.stat-bar', ['title' => 'Blocked Shots', 'statField' => 'blocked_shots'])
-                        @include('fixtures.partials.stat-bar', ['title' => 'Shots Inside Box', 'statField' => 'shots_inside_box'])
-                        @include('fixtures.partials.stat-bar', ['title' => 'Shots Outside Box', 'statField' => 'shots_outside_box'])
-                        @include('fixtures.partials.stat-bar', ['title' => 'Fouls', 'statField' => 'fouls'])
-                        @include('fixtures.partials.stat-bar', ['title' => 'Corners', 'statField' => 'corners'])
-                        @include('fixtures.partials.stat-bar', ['title' => 'Offsides', 'statField' => 'offsides'])
-                        @include('fixtures.partials.stat-bar', ['title' => 'Possession', 'statField' => 'possession', 'isPercentage' => true])
-                        @include('fixtures.partials.stat-bar', ['title' => 'Yellow Cards', 'statField' => 'yellow_cards'])
-{{--                        @include('fixtures.partials.stat-bar', ['title' => 'Red Cards', 'statField' => 'red_cards'])--}}
-                        @include('fixtures.partials.stat-bar', ['title' => 'Saves', 'statField' => 'saves'])
-                        @include('fixtures.partials.stat-bar', ['title' => 'Total Passes', 'statField' => 'total_passes'])
-                        @include('fixtures.partials.stat-bar', ['title' => 'Passes Completed', 'statField' => 'passes_completed'])
-                        @include('fixtures.partials.stat-bar', ['title' => 'Pass Accuracy', 'statField' => 'pass_accuracy', 'isPercentage' => true])
-{{--                        @include('fixtures.partials.stat-bar', ['title' => 'Expected Goals', 'statField' => 'expected_goals'])--}}
+                        @if (count($fixture->statistics))
+                            @include('fixtures.partials.stat-bar', ['title' => 'Shots on Goal', 'statField' => 'shots_on_goal'])
+                            @include('fixtures.partials.stat-bar', ['title' => 'Shots off Goal', 'statField' => 'shots_off_goal'])
+                            @include('fixtures.partials.stat-bar', ['title' => 'Total Shots', 'statField' => 'total_shots'])
+                            @include('fixtures.partials.stat-bar', ['title' => 'Blocked Shots', 'statField' => 'blocked_shots'])
+                            @include('fixtures.partials.stat-bar', ['title' => 'Shots Inside Box', 'statField' => 'shots_inside_box'])
+                            @include('fixtures.partials.stat-bar', ['title' => 'Shots Outside Box', 'statField' => 'shots_outside_box'])
+                            @include('fixtures.partials.stat-bar', ['title' => 'Fouls', 'statField' => 'fouls'])
+                            @include('fixtures.partials.stat-bar', ['title' => 'Corners', 'statField' => 'corners'])
+                            @include('fixtures.partials.stat-bar', ['title' => 'Offsides', 'statField' => 'offsides'])
+                            @include('fixtures.partials.stat-bar', ['title' => 'Possession', 'statField' => 'possession', 'isPercentage' => true])
+                            @include('fixtures.partials.stat-bar', ['title' => 'Yellow Cards', 'statField' => 'yellow_cards'])
+    {{--                        @include('fixtures.partials.stat-bar', ['title' => 'Red Cards', 'statField' => 'red_cards'])--}}
+                            @include('fixtures.partials.stat-bar', ['title' => 'Saves', 'statField' => 'saves'])
+                            @include('fixtures.partials.stat-bar', ['title' => 'Total Passes', 'statField' => 'total_passes'])
+                            @include('fixtures.partials.stat-bar', ['title' => 'Passes Completed', 'statField' => 'passes_completed'])
+                            @include('fixtures.partials.stat-bar', ['title' => 'Pass Accuracy', 'statField' => 'pass_accuracy', 'isPercentage' => true])
+    {{--                        @include('fixtures.partials.stat-bar', ['title' => 'Expected Goals', 'statField' => 'expected_goals'])--}}
+                        @else
+                            No statistics available yet
+                        @endif
                     </x-panel>
                 </div>
             </div>
