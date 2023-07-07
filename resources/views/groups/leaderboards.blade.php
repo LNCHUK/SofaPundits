b68e6147a698aeee590108959f7337fc388b5f8f
Tom Lynch <tom@lnch.co.uk>
2023-07-07 21:15:42 +0100

    Add overall leaderboard

--- resources/views/groups/leaderboards.blade.php
+++ resources/views/groups/leaderboards.blade.php
@@ -18,6 +18,78 @@
     <div class="py-12">
         <x-container>
 
+            <div class="mb-12">
+                <h2 class="mb-4 text-lg font-bold">Final Leaderboard</h2>
+                <table class="min-w-full divide-y divide-gray-300">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th scope="col" class="w-[5rem] px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500">
+                                Pos
+                            </th>
+                            <th scope="col" class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500">
+                                Player
+                            </th>
+                            <th scope="col" class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500">
+                                Total Score
+                            </th>
+                            <th scope="col" class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500">
+                                Correct Scores
+                            </th>
+                            <th scope="col" class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500">
+                                Max 3pts in a Week
+                            </th>
+                            <th scope="col" class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500">
+                                Backed Team Results
+                            </th>
+                            <th scope="col" class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500">
+                                Weeks Won
+                            </th>
+                            <th scope="col" class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500">
+                                Weeks Tied
+                            </th>
+                            <th scope="col" class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500">
+                                Max Week Score
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-200 bg-white">
+                    @foreach ($group->getLeagueTableData() as $data)
+                        <tr @if($data->position === 1) class="bg-gradient-to-l from-orange-100 to-transparent" @endif>
+                            <td class="whitespace-nowrap text-center px-2 py-2 text-sm text-gray-500">
+                                <span class="font-premier-league text-lg @if ($data->position === 1) correct-score-colour @endif drop-shadow-sm">
+                                    {{ $data->position }}
+                                </span>
+                            </td>
+                            <td class="whitespace-nowrap font-premier-league text-left px-3 py-3 text-sm text-gray-500">
+                                {{ $data->user->name }}
+                            </td>
+                            <td class="whitespace-nowrap font-premier-league text-center px-3 py-3 text-sm text-gray-500">
+                                {{ $data->total_points }}
+                            </td>
+                            <td class="whitespace-nowrap font-premier-league text-center px-3 py-3 text-sm text-gray-500">
+                                {{ $totalCorrectResultsTable->firstWhere('user_id', $data->user->id)?->score }}
+                            </td>
+                            <td class="whitespace-nowrap font-premier-league text-center px-3 py-3 text-sm text-gray-500">
+                                {{ $mostCorrectScoresInAWeek->firstWhere('user_id', $data->user->id)?->score }}
+                            </td>
+                            <td class="whitespace-nowrap font-premier-league text-center px-3 py-3 text-sm text-gray-500">
+                                {{ $backedTeamWins->get($data->user->id)['score'] ?? 0 }}
+                            </td>
+                            <td class="whitespace-nowrap font-premier-league text-center px-3 py-3 text-sm text-gray-500">
+                                {{ $mostWeeksWon->firstWhere('id', $data->user->id)['weeks_won'] }}
+                            </td>
+                            <td class="whitespace-nowrap font-premier-league text-center px-3 py-3 text-sm text-gray-500">
+                                {{ $mostWeeksTied->firstWhere('id', $data->user->id)['weeks_tied'] }}
+                            </td>
+                            <td class="whitespace-nowrap font-premier-league text-center px-3 py-3 text-sm text-gray-500">
+                                {{ $highestWeeklyScores->firstWhere('id', $data->user->id)['highest_weekly_score'] }}
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+
             <div class="md:flex gap-x-6 gap-y-6 mb-8">
                 <div class="md:w-1/3">
                     <h2 class="mb-4 text-lg font-bold">Total Correct Scores</h2>
