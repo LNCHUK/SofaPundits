97cb4823f456a9e22750a5ef8aa3ee120f754f5d
Tom Lynch <tom@lnch.co.uk>
2023-08-04 09:34:42 +0100

    Add fix to ignore unstarted or cancelled matches in points view

---
+++ database/migrations/2023_08_04_093110_update_correct_scores_view_to_ignore_unstarted_games.php
@@ -0,0 +1,90 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement($this->updateView());
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement($this->revertUpdatedView());
+    }
+
+    /**
+     * Returns the SQL to create the view.
+     *
+     * @return void
+     */
+    private function updateView(): string
+    {
+        return <<<SQL
+            ALTER
+                ALGORITHM=MERGE
+            VIEW `user_prediction_scores` AS
+                SELECT user_predictions.id,
+                       user_predictions.gameweek_id,
+                       user_predictions.user_id,
+                       user_predictions.fixture_id,
+                (
+                    CASE
+                        WHEN user_predictions.home_score = JSON_EXTRACT(`fixtures`.`score` , '$.fulltime.home') AND user_predictions.away_score = JSON_EXTRACT(`fixtures`.`score` , '$.fulltime.away') THEN 3
+                        WHEN (user_predictions.home_score > user_predictions.away_score AND JSON_EXTRACT(`fixtures`.`score` , '$.fulltime.home') > JSON_EXTRACT(`fixtures`.`score` , '$.fulltime.away'))
+                        OR (user_predictions.home_score < user_predictions.away_score AND JSON_EXTRACT(`fixtures`.`score` , '$.fulltime.home') < JSON_EXTRACT(`fixtures`.`score` , '$.fulltime.away'))
+                        OR (user_predictions.home_score = user_predictions.away_score AND JSON_EXTRACT(`fixtures`.`score` , '$.fulltime.home') = JSON_EXTRACT(`fixtures`.`score` , '$.fulltime.away'))
+                            THEN 1
+                        ELSE 0
+                    END
+                ) AS points
+                FROM user_predictions
+                INNER JOIN (
+                    SELECT * FROM fixtures WHERE status_code NOT IN ('TBD', 'NS', 'SUSP', 'PST', 'CANC', 'ABD', 'AWD', 'WO')
+                ) AS fixtures ON fixtures.id = user_predictions.fixture_id
+            SQL;
+    }
+
+    /**
+     * Returns the SQL to drop the view.
+     *
+     * @return void
+     */
+    private function revertUpdatedView(): string
+    {
+        return <<<SQL
+            ALTER
+                ALGORITHM=MERGE
+            VIEW `user_prediction_scores` AS
+                SELECT user_predictions.id,
+                       user_predictions.gameweek_id,
+                       user_predictions.user_id,
+                       user_predictions.fixture_id,
+                (
+                    CASE
+                        WHEN user_predictions.home_score = JSON_EXTRACT(`fixtures`.`score` , '$.fulltime.home') AND user_predictions.away_score = JSON_EXTRACT(`fixtures`.`score` , '$.fulltime.away') THEN 3
+                        WHEN (user_predictions.home_score > user_predictions.away_score AND JSON_EXTRACT(`fixtures`.`score` , '$.fulltime.home') > JSON_EXTRACT(`fixtures`.`score` , '$.fulltime.away'))
+                        OR (user_predictions.home_score < user_predictions.away_score AND JSON_EXTRACT(`fixtures`.`score` , '$.fulltime.home') < JSON_EXTRACT(`fixtures`.`score` , '$.fulltime.away'))
+                        OR (user_predictions.home_score = user_predictions.away_score AND JSON_EXTRACT(`fixtures`.`score` , '$.fulltime.home') = JSON_EXTRACT(`fixtures`.`score` , '$.fulltime.away'))
+                            THEN 1
+                        ELSE 0
+                    END
+                ) AS points
+                FROM user_predictions
+                INNER JOIN fixtures ON fixtures.id = user_predictions.fixture_id
+            SQL;
+    }
+};
